73d13af02dd5467f145b8a90e8396ce326565064
Matthew Scaperoth <mscapero@gwu.edu>
2014-08-18 11:02:41 -0400

    working with production directory Signed-off-by:Matthew Scaperoth <mscapero@gwu.edu>

--- protected/components/ApiHelper.php
+++ protected/components/ApiHelper.php
@@ -229,7 +229,7 @@ class ApiHelper extends CHtml {
         switch ($param) {
             case 'gadgets':
                 $service_details['local_file'] = self::_create_full_path(self::GADGETS_LOCAL);
-                $service_details['shared_file'] = (self::TEST?self::GADGETS_SHARED:self::_create_full_path(self::GADGETS_SHARED));
+                $service_details['shared_file'] = (self::TEST?self::_create_full_path(self::GADGETS_SHARED):self::GADGETS_SHARED);
                 $service_details['bucket'] = self::_create_full_path(self::GADGETS_BUCKET, true);
                 $service_details['database'] = Yii::app()->mongodb->gadgets;
                 break;
