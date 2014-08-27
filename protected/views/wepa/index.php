c577fe8101d92eefb98ec4c61321ce781826f7df
Matthew Scaperoth <mscapero@gwu.edu>
2014-08-27 12:49:38 -0400

    consolodated javascript and general functions into global files (instead of gadget_* or wepa_*) Signed-off-by:Matthew Scaperoth <mscapero@gwu.edu>

--- protected/views/wepa/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* @var $this WepaController */
-
-$this->breadcrumbs=array(
-	'Wepa',
-);
-?>
-<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
-
--- themes/bootstrap/assets/js/stellar/script.js
+++
@@ -1,8 +0,0 @@
-$.stellar.scrollProperty.transform = {
-  getLeft: function($element) {
-    return parseInt($element.css('margin-left'), 10) * -1;
-  },
-  getTop: function($element) {
-    return parseInt($element.css('margin-top'), 10) * -1;
-  }
-}
